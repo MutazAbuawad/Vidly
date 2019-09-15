e3cadfa08038544df981a161728f52094f01174b
MutazX Abu Awwad <mutazx.abu.awwad@intel.com>
2019-09-15 17:32:02 +0300

    Reference Docs

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ const dbDebugger = require('debug')('app:db');
 const conf = require('config');
 const morgan = require('morgan');
 const logger = require('./middlewares/logger');
-const express = require("express"); // returns a function
+const express = require('express'); // returns a function
 const mongooese = require('mongoose');
 
-const courses = require('./routes/courses')
+const courses = require('./routes/courses');
 const home = require('./routes/home');
-
 
 const app = express();
 
@@ -17,8 +16,8 @@ app.set('view engine', 'pug');
 app.set('views', './views'); // default
 
 // add json middleware of express to enable parsing the body of req
-// populate req.body property 
-app.use(express.json());  // for parsing application/json
+// populate req.body property
+app.use(express.json()); // for parsing application/json
 // to enable key=value&key=value in req route and populate req.body
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 // to enable serving static files
@@ -39,21 +38,40 @@ console.log(conf.get('mail.password'));
 app.use('/api/courses', courses);
 app.use('/', home);
 
-mongooese.connect('mongodb://localhost/coursesDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongooese
+  .connect('mongodb://localhost/coursesDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => dbDebugger('Connected to MongoDB'))
-  .catch((err) => console.error('Could not connected to MongoDB', err.message));
+  .catch(err => console.error('Could not connected to MongoDB', err.message));
+
+// const courseSchema = mongooese.Schema({
+//   name: { type: String, required: true },
+//   author: String,
+//   tags: [String],
+//   price: Number,
+//   date: { type: String, default: Date.now },
+//   isPublished: Boolean
+// });
 
 const courseSchema = mongooese.Schema({
-  name: { type: String, required: true },
-  author: String,
-  tags: [String],
-  price: Number,
-  date: { type: String, default: Date.now },
-  isPublished: Boolean
+  name : String,
+  author: {
+    type: mongooese.Schema.Types.ObjectId,
+    ref: 'Author'
+  }
 });
 
 // mongoose.model compiles the cousreSchema into Course class so can instantiate an objects/documents from that class
 const Course = mongooese.model('Course', courseSchema);
+
+const Author = mongooese.model('Author',mongooese.Schema({
+    name: String,
+    bio: String,
+    website: String
+  })
+);
 
 // Create a course
 async function createCourse() {
@@ -69,18 +87,41 @@ async function createCourse() {
   console.log(result);
 }
 
+// Create an author
+async function createAuthor(name, bio, website) {
+  const author = new Author({
+    name,
+    bio,
+    website
+  });
+
+  const result = await author.save();
+  console.log(result);
+}
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
 // Get courses
 async function getCourses() {
   const courses = await Course
-    //.find({ author: 'Mosh', isPublished: true })
-    .find({ author: 'Mosh', price: { $gte: 10, $lte: 20 } }) // query courses with prices between 10 and 20
+    .find()
+    // .find({ author: 'Mosh', isPublished: true })
+    // .find({ author: 'Mosh', price: { $gte: 10, $lte: 20 } }) // query courses with prices between 10 and 20
     //.find({ author: 'Mosh', price : { $in : [10, 20, 30]}) // query courses with prices 10, 20 or 30
     //.find()
     //.or([{ author: 'Mosh' }, { isPublished: true }])
     //.and([{ author: 'Mosh' }, { isPublished: true }])
-    .limit(10)
-    .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    // .limit(10)
+    // .sort({ name: 1 })
+    .select({ name: 1});
   console.log(courses);
 }
 
@@ -94,10 +135,9 @@ async function getCountOfCourses() {
 }
 
 // pagination
-// /api/courses?pageNumber=2&pageSize=10 
+// /api/courses?pageNumber=2&pageSize=10
 async function getCoursesInPage(pageNumber, pageSize) {
-  const courses = await Course
-    .find()
+  const courses = await Course.find()
     .skip((pageNumber - 1) * pageSize)
     .limit(pageSize);
   console.log(courses);
@@ -127,12 +167,16 @@ async function updayeCourseSecondApproach(id) {
   // second approach: update first
   // update directly
   // optionally: get the updated documnet
-  const course = await Course.findByIdAndUpdate(id, {
-    $set: {
-      author: 'Jason',
-      isPublished: true
-    }
-  }, { new: true });
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        author: 'Jason',
+        isPublished: true
+      }
+    },
+    { new: true }
+  );
 
   console.log(course);
 }
@@ -148,7 +192,10 @@ async function deleteCourse(id) {
 // getCoursesInPage(1, 2);
 // updateCourseFirstApproach('5d7be96c6d3123296407cfdc');
 // updayeCourseSecondApproach('5d7be96c6d3123296407cfdc')
-deleteCourse('5d7bef90e9a1bf15385eacd7');
+// deleteCourse('5d7bef90e9a1bf15385eacd7');
+createAuthor('Mosh','My bio','My website');
+// createCourse('Node Course', 'authorId');
+// getCourses();
 
 // add a port so server can listening on
 const port = process.env.PORT || 3000;
